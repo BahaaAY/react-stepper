6bf1672d41070c1bc38d03cff47356d53229b342
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-09-16 09:14:06 +0300

    created static design 🗿

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p>Hello World!</p>
+      <div className="stepper-container">
+        <ul className="stepper">
+          <li className="step step-active">1</li>
+          <li className="step">2</li>
+          <li className="step">3</li>
+        </ul>
+        <h1 className="message">Step 1</h1>
+        <div className="buttons-container">
+          <button>Previous</button>
+          <button>Next</button>
+        </div>
+      </div>
     </>
   );
 }
