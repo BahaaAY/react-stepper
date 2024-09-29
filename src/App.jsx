f2a3b95b6d11145d157c5a4d89511530910bc16a
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-09-29 13:10:14 +0300

    added previous and next functionality ⚙️

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 const messages = ["Learn React", "Master React", "Build React Apps"];
 import { StepperList } from "./components/StepperList";
 function App() {
-  let step = 1;
+  const [step, setStep] = useState(1);
+  const maxStep = messages.length;
+  const minStep = 1;
   return (
     <>
       <div className="stepper-container">
         <StepperList stepsCount={messages.length} currentStep={step} />
         <h1 className="message">{messages[step - 1]}</h1>
         <div className="buttons-container">
-          <button>Previous</button>
-          <button>Next</button>
+          <button
+            disabled={step === minStep} // Disable button if at the first step
+            onClick={() =>
+              setStep((prevStep) => Math.max(prevStep - 1, minStep))
+            }
+          >
+            Previous
+          </button>
+          <button
+            disabled={step === maxStep} // Disable button if at the last step
+            onClick={() =>
+              setStep((prevStep) => Math.min(prevStep + 1, maxStep))
+            }
+          >
+            Next
+          </button>
         </div>
       </div>
     </>
